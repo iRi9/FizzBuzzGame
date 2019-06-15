54e3edb7c290c1849d8a4fe34c43e1c1c4d7e2db
_giri <giribahari45@gmail.com>
2019-06-15 22:25:16 +0700

    brain before beauty

---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,40 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by ebdesk-066 on 15/06/19.
+//  Copyright © 2019 irig.com. All rights reserved.
+//
+
+import Foundation
+
+class Brain {
+    
+    func isDivisibleByThree(number: Int) -> Bool {
+        return isDivisibleBy(divisor: 3, number: number)
+    }
+    
+    func isDivisibleByFive(number: Int) -> Bool {
+        return isDivisibleBy(divisor: 5, number: number)
+    }
+    
+    func isDivisibleByFifteen(number: Int) -> Bool {
+        return isDivisibleBy(divisor: 15, number: number)
+    }
+    
+    func isDivisibleBy(divisor: Int, number: Int) -> Bool {
+        return number % divisor == 0
+    }
+    
+    func check(number: Int) -> String {
+        if isDivisibleByFifteen(number: number){
+            return "FizzBuzz"
+        }else if isDivisibleByThree(number: number) {
+            return "Fizz"
+        } else if isDivisibleByFive(number: number) {
+            return "Buzz"
+        } else {
+            return "\(number)"
+        }
+    }
+}
--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -7,8 +7,11 @@
 //
 
 import XCTest
+@testable import FizzBuzz
 
 class BrainTests: XCTestCase {
+    
+    let brain = Brain()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,16 +21,54 @@ class BrainTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testIsDivisibleByThree() {
+        let result = brain.isDivisibleByThree(number: 3)
+        XCTAssertEqual(result, true)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testIsNotDivisibleByThree() {
+        let result = brain.isDivisibleByThree(number: 1)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIsDivisibleByFive() {
+        let result = brain.isDivisibleByFive(number: 5)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsNotDivisibleByFive() {
+        let result = brain.isDivisibleByFive(number: 3)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIsDivisibleByFiveteen() {
+        let result = brain.isDivisibleByFifteen(number: 15)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsNotDivisibleByFiveteen() {
+        let result = brain.isDivisibleByFifteen(number: 1)
+        XCTAssertEqual(result, false)
+    }
+    
+    func testSayFizz() {
+        let result = brain.check(number: 3)
+        XCTAssertEqual(result, "Fizz")
+    }
+    
+    func testSayBuzz() {
+        let result = brain.check(number: 5)
+        XCTAssertEqual(result, "Buzz")
+    }
+    
+    func testSayFizzBuzz() {
+        let result = brain.check(number: 15)
+        XCTAssertEqual(result, "FizzBuzz")
+    }
+    
+    func testSayOne() {
+        let result = brain.check(number: 1)
+        XCTAssertEqual(result, "1")
     }
 
 }
