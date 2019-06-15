b0b0e88ca84e2e8c394f58c0786395482917a7f4
_giri <giribahari45@gmail.com>
2019-06-15 21:07:27 +0700

    Initial Commit

---
+++ FizzBuzz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FizzBuzz
+//
+//  Created by ebdesk-066 on 15/06/19.
+//  Copyright © 2019 irig.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
